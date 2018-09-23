19894fa1cf067ac1ac126cec54de153af9aea551
d876811 <mahendrahiraman.bagul@team.telstra.com>
2018-09-23 11:40:52 +0530

    image pagination done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { DeviceDetailComponent } from './device-detail/device-detail.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ImagePaginationComponent } from './image-pagination/image-pagination.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeviceDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ImagePaginationComponent
   ],
   imports: [
-    BrowserModule, NgxPaginationModule
+    BrowserModule, NgxPaginationModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/device-detail/device-detail.component.ts
+++ src/app/device-detail/device-detail.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { tap, map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import { PaginationInstance } from 'ngx-pagination';
+import { DeviceDetailService, Device } from '../device-detail.service';
 
 interface IServerResponse {
-  devices: string[];
+  devices: Device[];
   total: number;
 }
 
@@ -14,136 +12,34 @@ interface IServerResponse {
   styleUrls: ['./device-detail.component.css']
 })
 export class DeviceDetailComponent implements OnInit {
-  p = 1;
   devices: any[];
   loading: boolean;
-  public filter = '';
-  public maxSize = 7;
-  public directionLinks = false;
-  public autoHide = false;
+  currentPage: 1;
   public responsive = false;
-  public config: PaginationInstance = {
-    id: 'advanced',
-    itemsPerPage: 10,
-    currentPage: 1
-  };
-  public labels1: any = {
-    previousLabel: '',
-    nextLabel: '',
-    screenReaderPaginationLabel: 'Pagination',
-    screenReaderPageLabel: 'page',
-    screenReaderCurrentLabel: `You're on page`
-  };
-  public labels3: any = {
-    previousLabel: '',
-    nextLabel: '',
-    screenReaderPaginationLabel: 'Pagination',
-    screenReaderPageLabel: 'page',
-    screenReaderCurrentLabel: `You're on page`
-  };
-  public labels2: any = {
-    previousLabel: 'Previous',
-    nextLabel: 'Next',
-    screenReaderPaginationLabel: 'Pagination',
-    screenReaderPageLabel: 'page',
-    screenReaderCurrentLabel: `You're on page`
-  };
+  itemsPerPage = 6;
+  // checked by hitting api without filters 1000
+  maxPages = 60 / this.itemsPerPage;
 
-  onPageChange(number: number) {
-    console.log('change to page', number);
-    this.config.currentPage = number;
-  }
-
-  constructor() {
-    this.devices =
-      [
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed52c'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:18:43.825Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed52b'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:17:31.402Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed52a'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:16:18.960Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed52f'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:15:06.522Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed528'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:13:54.183Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed527'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:12:41.739Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed526'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:11:29.326Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed525'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:10:16.888Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed524'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:09:04.489Z',
-          'moisture': 94
-        },
-        {
-          '_id': {
-            '$oid': '5b39eb1e1ad8b6634b7ed523'
-          },
-          'deviceId': 'AF01',
-          'timestamp': '2018-06-09T05:07:52.049Z',
-          'moisture': 94
-        }
-      ];
+  constructor(private deviceDetailService: DeviceDetailService) {
+    this.loadDeviceDetailsByPage(this.itemsPerPage, 0);
   }
 
   ngOnInit() {
-    this.getPage(1);
+  }
+  pageChanged(event) {
+    this.currentPage = event.page || 1;
+    this.itemsPerPage = event.itemsPerPage;
+    this.loadDeviceDetailsByPage(this.itemsPerPage, this.currentPage);
   }
 
-  getPage(page: number) {
+  loadDeviceDetailsByPage(limit: number, skip: number) {
+    this.loading = true;
+    this.deviceDetailService.getDeviceDetailsList(limit, skip).subscribe(data => {
+      this.devices = data;
+      this.loading = false;
+    }, error => {
+      this.loading = false;
+      console.log(error);
+    });
   }
 }
